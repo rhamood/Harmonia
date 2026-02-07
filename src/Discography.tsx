a0af89b343e1a8794f119f64241c5bc122050885
tckillam <tckillam@gmail.com>
2026-02-06 23:12:08 -0500

    very light disco edits
    ill work more this weekend shawtys

--- src/Discography.tsx
+++ src/Discography.tsx
@@ -2,6 +2,8 @@ function DiscographyPage() {
   return (
     <div className='bg-[#D496BB] min-h-screen'>
       <h1 className='text-6xl font-bold text-center mt-8 text-white'> DISCOGRAPHY PAGE</h1>
+      <p className='text-desc font-bold text-center mt-8 text-white'> Find Your Next Fave at the Disco </p>
+      <button className='text-add font-bold mt-8 text-white border border-white bg-[#EFBFE9] block mx-auto'> Add Album </button>
     </div>
   )
 }
