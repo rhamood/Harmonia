cb16aa55253bf865a7845c5b144a3fb561ab9641
tckillam <tckillam@gmail.com>
2026-02-08 11:53:40 -0500

    Update Discography.tsx
    I think I need to actually work on the databases and api stuff to fully finish this page

--- src/Discography.tsx
+++ src/Discography.tsx
@@ -4,7 +4,52 @@ function DiscographyPage() {
       <h1 className='text-6xl font-bold text-center mt-8 text-white'> DISCOGRAPHY PAGE</h1>
       <p className='text-desc font-bold text-center mt-8 text-white'> Find Your Next Fave at the Disco </p>
       <button className='text-add font-bold mt-8 text-white border border-white bg-[#EFBFE9] block mx-auto'> Add Album </button>
+      <div className='px-32'>
+      <div className='w-full space-x-8 flex items-center justify-center mt-12 pb-12'>
+             <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+             <br></br>
+             <h3 className='font-bold text-2xl'> Album Name </h3>
+             <br></br>
+             <h3 className='font-bold text-1xl'> Artist Name </h3>
+             <h3 className='font-bold text-1xl'> Album Rating </h3>
+            </div>
+             <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+             <br></br>
+             <h3 className='font-bold text-2xl'> Album Name </h3>
+             <br></br>
+             <h3 className='font-bold text-1xl'> Artist Name </h3>
+             <h3 className='font-bold text-1xl'> Album Rating </h3>
+            </div>
+             <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+             <br></br>
+             <h3 className='font-bold text-2xl'> Album Name </h3>
+             <br></br>
+             <h3 className='font-bold text-1xl'> Artist Name </h3>
+             <h3 className='font-bold text-1xl'> Album Rating </h3>
+            </div>
+            <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+             <br></br>
+             <h3 className='font-bold text-2xl'> Album Name </h3>
+             <br></br>
+             <h3 className='font-bold text-1xl'> Artist Name </h3>
+             <h3 className='font-bold text-1xl'> Album Rating </h3>
+            </div>
+            <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+             <br></br>
+             <h3 className='font-bold text-2xl'> Album Name </h3>
+             <br></br>
+             <h3 className='font-bold text-1xl'> Artist Name </h3>
+             <h3 className='font-bold text-1xl'> Album Rating </h3>
+            </div>
+          </div> 
+      </div>           
     </div>
+    
   )
 }
 export default DiscographyPage;
