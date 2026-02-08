d648a6931a7b63f3e8ac584f9cc58501ba012cdf
tckillam <tckillam@gmail.com>
2026-02-08 12:27:34 -0500

    Update Discography.tsx

--- src/Discography.tsx
+++ src/Discography.tsx
@@ -3,97 +3,126 @@ function DiscographyPage() {
     <div className='bg-[#D496BB] min-h-screen'>
       <h1 className='text-6xl font-bold text-center mt-8 text-white'> DISCOGRAPHY PAGE</h1>
       <p className='text-desc font-bold text-center mt-8 text-white'> Find Your Next Fave at the Disco </p>
-      <button className='text-add font-bold mt-8 text-white border border-white bg-[#EFBFE9] block mx-auto'> Add Album </button>
+      <button className='text-add font-bold mt-8 text-white border border-white bg-[#EFBFE9] block mx-auto'> + Add Album </button>
       <div className='px-32'>
       
 {/* The first row of suggested albums */}
       <div className='w-full space-x-8 flex items-center justify-center mt-12 pb-12'>
-             <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
-             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+             
+             {/* Album 1 */}
+             <div className='w-1/5 h-120 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-1/2 bg-black flex justify-center'> </div>
              <br></br>
              <h3 className='font-bold text-2xl'> Album Name </h3>
              <br></br>
              <h3 className='font-bold text-1xl'> Artist Name </h3>
              <h3 className='font-bold text-1xl'> Album Rating </h3>
+             <button className='text-add font-bold mt-4 text-white border border-white bg-[#EFBFE9] block mx-auto'> + Add Album </button>
             </div>
-             <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
-             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+
+            {/* Album 2 */}
+             <div className='w-1/5 h-120 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-1/2 bg-black flex justify-center'> </div>
              <br></br>
              <h3 className='font-bold text-2xl'> Album Name </h3>
              <br></br>
              <h3 className='font-bold text-1xl'> Artist Name </h3>
              <h3 className='font-bold text-1xl'> Album Rating </h3>
+             <button className='text-add font-bold mt-4 text-white border border-white bg-[#EFBFE9] block mx-auto'> + Add Album </button>
             </div>
-             <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
-             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+
+            {/* Album 3 */}
+             <div className='w-1/5 h-120 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-1/2 bg-black flex justify-center'> </div>
              <br></br>
              <h3 className='font-bold text-2xl'> Album Name </h3>
              <br></br>
              <h3 className='font-bold text-1xl'> Artist Name </h3>
              <h3 className='font-bold text-1xl'> Album Rating </h3>
+             <button className='text-add font-bold mt-4 text-white border border-white bg-[#EFBFE9] block mx-auto'> + Add Album </button>
             </div>
-            <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
-             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+
+            {/* Album 4 */}
+             <div className='w-1/5 h-120 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-1/2 bg-black flex justify-center'> </div>
              <br></br>
              <h3 className='font-bold text-2xl'> Album Name </h3>
              <br></br>
              <h3 className='font-bold text-1xl'> Artist Name </h3>
              <h3 className='font-bold text-1xl'> Album Rating </h3>
+             <button className='text-add font-bold mt-4 text-white border border-white bg-[#EFBFE9] block mx-auto'> + Add Album </button>
             </div>
-            <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
-             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+
+            {/* Album 5 */}
+             <div className='w-1/5 h-120 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-1/2 bg-black flex justify-center'> </div>
              <br></br>
              <h3 className='font-bold text-2xl'> Album Name </h3>
              <br></br>
              <h3 className='font-bold text-1xl'> Artist Name </h3>
              <h3 className='font-bold text-1xl'> Album Rating </h3>
+             <button className='text-add font-bold mt-4 text-white border border-white bg-[#EFBFE9] block mx-auto'> + Add Album </button>
             </div>
           </div> 
 
   {/* The second row of suggested albums */}
-      <div className='w-full space-x-8 flex items-center justify-center mt-12 pb-12'>
-             <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
-             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+        <div className='w-full space-x-8 flex items-center justify-center mt-12 pb-12'>
+             
+             {/* Album 1 */}
+             <div className='w-1/5 h-120 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-1/2 bg-black flex justify-center'> </div>
              <br></br>
              <h3 className='font-bold text-2xl'> Album Name </h3>
              <br></br>
              <h3 className='font-bold text-1xl'> Artist Name </h3>
              <h3 className='font-bold text-1xl'> Album Rating </h3>
+             <button className='text-add font-bold mt-4 text-white border border-white bg-[#EFBFE9] block mx-auto'> + Add Album </button>
             </div>
-             <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
-             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+
+            {/* Album 2 */}
+             <div className='w-1/5 h-120 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-1/2 bg-black flex justify-center'> </div>
              <br></br>
              <h3 className='font-bold text-2xl'> Album Name </h3>
              <br></br>
              <h3 className='font-bold text-1xl'> Artist Name </h3>
              <h3 className='font-bold text-1xl'> Album Rating </h3>
+             <button className='text-add font-bold mt-4 text-white border border-white bg-[#EFBFE9] block mx-auto'> + Add Album </button>
             </div>
-             <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
-             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+
+            {/* Album 3 */}
+             <div className='w-1/5 h-120 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-1/2 bg-black flex justify-center'> </div>
              <br></br>
              <h3 className='font-bold text-2xl'> Album Name </h3>
              <br></br>
              <h3 className='font-bold text-1xl'> Artist Name </h3>
              <h3 className='font-bold text-1xl'> Album Rating </h3>
+             <button className='text-add font-bold mt-4 text-white border border-white bg-[#EFBFE9] block mx-auto'> + Add Album </button>
             </div>
-            <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
-             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+
+            {/* Album 4 */}
+             <div className='w-1/5 h-120 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-1/2 bg-black flex justify-center'> </div>
              <br></br>
              <h3 className='font-bold text-2xl'> Album Name </h3>
              <br></br>
              <h3 className='font-bold text-1xl'> Artist Name </h3>
              <h3 className='font-bold text-1xl'> Album Rating </h3>
+             <button className='text-add font-bold mt-4 text-white border border-white bg-[#EFBFE9] block mx-auto'> + Add Album </button>
             </div>
-            <div className='w-1/5 h-100 bg-white flex justify-center flex flex-col items-center'>
-             <div className='w-4/5 h-3/5 bg-black flex justify-center'> </div>
+
+            {/* Album 5 */}
+             <div className='w-1/5 h-120 bg-white flex justify-center flex flex-col items-center'>
+             <div className='w-4/5 h-1/2 bg-black flex justify-center'> </div>
              <br></br>
              <h3 className='font-bold text-2xl'> Album Name </h3>
              <br></br>
              <h3 className='font-bold text-1xl'> Artist Name </h3>
              <h3 className='font-bold text-1xl'> Album Rating </h3>
+             <button className='text-add font-bold mt-4 text-white border border-white bg-[#EFBFE9] block mx-auto'> + Add Album </button>
             </div>
           </div> 
-
   
       </div>           
     </div>
