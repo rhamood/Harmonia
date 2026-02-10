14932bed9dbce1cba092f56d6ac4b00e41491310
Rana Hamood <ranahamood63@gmail.com>
2026-02-10 00:01:09 -0500

    adding routing navigation

---
+++ src/NavbarComponent.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function NavbarComponent() {
+  return (
+        <nav className='w-full bg-white p-4 flex justify-start space-x-4 text-[#D496BB]'> 
+            <ul className="flex space-x-4">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+                <li><Link to="/discography">Discography</Link></li>
+            </ul>
+        </nav>
+  )
+}
+
+export default NavbarComponent;
