b0464ab5bbe99abd358c98d0c4ecfd51dcb42d77
inayarajwani <47422650+inayarajwani@users.noreply.github.com>
2026-02-12 14:35:57 -0500

    New folder for site images+small changes

--- src/ProfilePage.tsx
+++ src/ProfilePage.tsx
@@ -4,7 +4,8 @@ function ProfilePage() {
   return (
     <div className='bg-[#D496BB] min-h-screen'>
       <NavbarComponent />
-      <h1 className='text-6xl font-bold text-center mt-8 text-white'> PROFILE PAGE</h1>
+      <link href = 'https://www.flaticon.com/free-icon/woman_6997662?term=profile&page=1&position=13&origin=search&related_id=6997662'></link>
+      <h1 className='text-6xl font-bold text-center mt-8 text-white'> Welcome Jane Doe</h1>
     </div>
   )
 }
