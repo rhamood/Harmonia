e80a009fe9f2ad584372487394166da20a0a2938
tckillam <tckillam@gmail.com>
2026-04-06 16:43:59 -0400

    Update FAQ.tsx

--- frontend/src/FAQ.tsx
+++ frontend/src/FAQ.tsx
@@ -17,7 +17,48 @@ function FAQPage() {
             <div className='bg-[#D496BB] min-h-full'>
                 <NavbarComponent />
                 <div className='px-32'>
-                    <h1 className='text-8xl font-bold text-center mt-8 text-white'> Frequently Asked Questions </h1>
+                    <h1 className='text-7xl font-bold text-center mt-8 text-white'> Frequently Asked Questions </h1>
+                </div>
+                <br></br>
+                <div className='p-4'>
+                    <div className='bg-white p-2 text-[#934774]'>
+                    <h2> Question:<br></br> How do I recover a deleted album? </h2>
+                    <h3> Answer: <br></br> Deleting an album will only remove it from your Profile. 
+                        You can go back to the Discography page and click the '+ Add' Button once again to recover
+                        the album with your original rating and review.
+                    </h3>
+                    </div>
+                    <br></br>
+                    
+                    <div className='bg-white p-2 text-[#934774]'>
+                    <h2> Question:<br></br> How do I see my score on the game page? </h2>
+                    <h3> Answer: <br></br> When you finish the game, your score will be displayed at the end. Correct answers will
+                         not be given so you can try again ;)
+                    </h3>
+                    </div>
+                    <br></br>
+
+                    <div className='bg-white p-2 text-[#934774]'>
+                    <h2> Question:<br></br> Can I change my album rating after my original score? </h2>
+                    <h3> Answer: <br></br> Yes you can. All you need to do is click the 'Rate Album' button 
+                         on your Profile under the album rating you want to update and you simply change your score.
+                    </h3>
+                    </div>
+                    <br></br>
+
+                    <div className='bg-white p-2 text-[#934774]'>
+                    <h2> Question:<br></br> question 4 </h2>
+                    <h3> Answer: <br></br> answer 4 <br></br><br></br>
+                    </h3>
+                    </div>
+                    <br></br>
+
+                    <div className='bg-white p-2 text-[#934774]'>
+                    <h2> Question:<br></br> question 5 </h2>
+                    <h3> Answer: <br></br> answer 5 <br></br><br></br>
+                    </h3>
+                    </div>
+                    <br></br>
                 </div>
                 
             </div>
